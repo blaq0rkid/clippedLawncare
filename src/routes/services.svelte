2ae450988fd7a1d703feea8a18b5fc6559f1a27d
Kelley <kurozato.candy@gmail.com>
2020-12-17 06:47:51 -0800

    Tightened up Services page

--- src/routes/services.svelte
+++ src/routes/services.svelte
@@ -9,31 +9,6 @@ body{
     background-color: #edeff0;
     font-family: 'Roboto', sans-serif;
 }
-/*header*/
-header{
-    background-color: #FBF9FF;
-    box-shadow: 0px 0px 20px rgba(0,0,0,.2);
-}
-header .logo{
-    font-size: 1.5em;
-    font-family: 'Raleway', sans-serif;
-}
-
-header li{
-    display: inline-block;
-}
-header a{
-    display: inline-block;
-    color: #0D1413;
-    text-decoration: none;
-    padding: 10px;
-    transition: background-color 0.4s;
-}
-header nav a:hover,
-header .active a{
-    background-color: rgba(0,0,0,.05);
-}
-/****header*/
 .container{
     max-width: 1000px;
     margin: auto;
@@ -42,7 +17,7 @@ header .active a{
 /*showcase*/
 #showcase{
     height: 300px;
-    background-image: url(../img/garden.jpg);
+    background-image: url(https://images.unsplash.com/photo-1578302758063-0ef3e048ca89?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NXx8Z2FyZGVufGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60);
     background-attachment: fixed;
     background-size: cover;
     position: relative;
@@ -93,13 +68,13 @@ header .active a{
     background-color: #fff; 
 }
 .service.one{
-    background-image:linear-gradient(rgba(0,0,0,.7),rgba(0,0,0,.6)), url(../img/s1.jpeg);  
+    background-image:linear-gradient(rgba(0,0,0,.7),rgba(0,0,0,.6)), url(https://images.unsplash.com/photo-1549276755-1bbd36282c6f?ixid=MXwxMjA3fDB8MHxzZWFyY2h8N3x8Z2FyZGVufGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60);  
 }
 .service.two{
-    background-image:linear-gradient(rgba(0,0,0,.7),rgba(0,0,0,.6)), url(../img/s2.jpeg);  
+    background-image:linear-gradient(rgba(0,0,0,.7),rgba(0,0,0,.6)), url(https://images.unsplash.com/photo-1550948390-6eb7fa773072?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTF8fGdhcmRlbnxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60);  
 }
 .service.three{
-    background-image:linear-gradient(rgba(0,0,0,.7),rgba(0,0,0,.6)), url(../img/s3.jpeg);  
+    background-image:linear-gradient(rgba(0,0,0,.7),rgba(0,0,0,.6)), url(https://images.unsplash.com/photo-1558293842-c0fd3db86157?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NjB8fGdhcmRlbnxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60);  
 }
 .service h3{
     font-size: 1.3em;
@@ -108,54 +83,9 @@ header .active a{
     margin-bottom: 5px;
 }
 
-/*footer*/
-footer{
-    background-color: #0D1413;
-    color: #fff;
-}
-footer div{
-    padding: 10px;
-}
-footer ul{
-    list-style-type: none;
-}
-footer a{
-    color: #fff;
-    padding: 10px 0px;
-}
-footer p{
-    padding: 10px 0px;
-}
-.news-letter input{
-    padding: 10px;
-    width: 100%;
-}
-.news-letter button{
-    border: none;
-    width: 40%;
-    margin: 20px 0px;
-    padding: 15px;
-    background-color: #55C18F;
-    color: #fff;
-    transition: box-shadow 0.5s;
-}
-
-.news-letter button:hover{
-    box-shadow: 0px 0px 10px rgba(0,0,0,.3);
-}
-/****footer*/
-
-
 /*media queries for tablets*/
 @media screen and (min-width: 750px){
-    header .logo{
-        display: inline-block;
-        width: 30%;
-    }
-    header nav{
-        display: inline-block;
-        width: 60%;
-    }
+
     .service{
         width: 47.5%;
         margin: 10px 0px;
@@ -180,32 +110,6 @@ footer p{
         margin: 15px 0px;
     }
     
-
-    footer .services-links,
-    footer .get-in-touch{
-        margin: 20px;
-        width: 40%;
-        display: inline-block;
-    }
-    footer .services-links a{
-        padding: 5px 0px;
-        display: inline-block;
-    }
-    footer .news-letter{
-        margin: 10px 20px;
-    }
-    footer .news-letter input{
-        width: 70%;
-    }
-    
-    footer .news-letter button{
-        margin-left: 10px;
-        width: 20%;
-        padding: 12px;
-    }
-    footer .copyright{
-        text-align: center;
-    }
 }
 
 </style>
@@ -230,51 +134,20 @@ footer p{
                     <div class="service one">
                         <h3>Consultation</h3>
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum cupiditate inventore laborum harum ad, tempore necessitatibus amet, autem nihil enim expedita aliquid mollitia nostrum quisquam beatae aliquam iusto repellendus nam.</p>
-                        <button class="call-to-action">Read more</button>
+                        <button class="call-to-action">Call Us For A Consultation</button>
                     </div>
                     <div class="service two">
                         <h3>Concept Design</h3>
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum expedita itaque deserunt incidunt ex nam quia architecto ea earum vero at optio laboriosam quas, beatae reiciendis aperiam libero blanditiis tempore.</p>
-                        <button class="call-to-action">Read more</button>
+                        <button class="call-to-action">Call Us For A Consultation</button>
                     </div>
                     <div class="service three">
                         <h3>Documentation</h3>
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum expedita itaque deserunt incidunt ex nam quia architecto ea earum vero at optio laboriosam quas, beatae reiciendis aperiam libero blanditiis tempore.</p>
-                        <button class="call-to-action">Read more</button>
+                        <button class="call-to-action">Call Us For A Consultation</button>
                     </div>
                 </div>
             </div>
-        </div>
-       
+        </div>       
     </section>
-    
-    <footer>
-        <div class="container">
-            <div class="services-links">
-                <h3>Services</h3>
-                <ul>
-                    <li><a href="">Fall &amp; spring clean up</a></li>
-                    <li><a href="">Lawn care</a></li>
-                    <li><a href="">Landscape design &amp; installation</a></li>
-                    <li><a href="">Plants, flowers, soils and mulches</a></li>
-                </ul>
-            </div>
-            <div class="get-in-touch">
-                <p>44 Shirley Ave. West Chicago, IL 60185</p>
-                <p>1-(900)-123-4567</p>
-                <p>Email: Mygarden@gmail.com</p>
-            </div>
-            <div class="news-letter">
-                <form action="">
-                    <input type="email" placeholder="Enter your email">
-                    <button type="submit">Subscribe</button>
-                </form>
-            </div>
-            <div class="copyright">
-                <p>Copyright &copy; 2018 Hichem Med.</p>
-            </div>
-        </div>
-        
-    </footer>
-    
 </body>
